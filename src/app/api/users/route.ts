30638b2114d1fc16744019dad3512a35b976e721
Monil072718 <patelmonil7908@gmail.com>
2026-02-12 10:59:57 +0530

    first commit

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,24 @@
+import dbConnect from "@/lib/mongodb";
+import User from "@/models/User";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  await dbConnect();
+  try {
+    const users = await User.find({});
+    return NextResponse.json({ success: true, data: users });
+  } catch (error) {
+    return NextResponse.json({ success: false }, { status: 400 });
+  }
+}
+
+export async function POST(request: Request) {
+  await dbConnect();
+  try {
+    const body = await request.json();
+    const user = await User.create(body);
+    return NextResponse.json({ success: true, data: user }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ success: false }, { status: 400 });
+  }
+}
---
+++ src/models/User.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide a name for this user.'],
+    maxlength: [60, 'Name cannot be more than 60 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide an email for this user.'],
+    maxlength: [60, 'Email cannot be more than 60 characters'],
+  },
+});
+
+export default mongoose.models.User || mongoose.model('User', UserSchema);
